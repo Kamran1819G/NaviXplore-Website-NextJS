4660ce73a17fc9aa76fb40a9d54bc3aaf1871e41
Kamran Khan <kamrankhan.sde@gmail.com>
2024-01-16 02:24:08 +0530

    Created Advertise with us page

---
+++ src/app/advertise-with-us/page.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AdvertiseWithUs = () => {
+  return (
+    <>
+      <div className="page-heading-section">
+        <span className="page-heading">Advertise With Us</span>
+      </div>
+      <div className="page-container mx-auto"></div>
+    </>
+  );
+};
+
+export default AdvertiseWithUs;
